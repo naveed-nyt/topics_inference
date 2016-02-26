1fe07b99d7cd7cc06a32ad44587791f9c0c930b1
Joe Cora <joseph.cora@nytimes.com>
2016-02-26 16:07:33 -0500

    Allow tester to use command line params

--- tester.py
+++ tester.py
@@ -1,15 +1,22 @@
+import sys
 from nmf_topics import NMFTopics
 from lda_topics import LDATopics
 
 # Location of the emails
 EMAIL_DATA_LOCATION = '/Users/206958/Desktop/Term_Topics/'
 
-# Load the email file
-email_file = EMAIL_DATA_LOCATION + 'email_export.xlsx'
+if __name__ == '__main__':
+	# Get command line params
+	model_type = sys.argv[1]
+	email_file = sys.argv[2]
 
-# Test topics
-tester = LDATopics(email_file)
+	# Load the proper model
+	if model_type.lower() == 'nmf':
+		processor = NMFTopics(EMAIL_DATA_LOCATION + email_file)
+	else:
+		processor = LDATopics(EMAIL_DATA_LOCATION + email_file)
 
-tester.process()
+	# Process the topics
+	processor.process()
 
-tester.print_topics(words_per_topic=7)
+	processor.print_topics(words_per_topic=7)
